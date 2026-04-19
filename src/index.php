fa2304821e423a1afa727fa11742f6c46777a915
Vittorione <vittorio.tiozzo.dev@gmail.com>
2026-04-19 23:18:45 +0200

    implemented homepage

---
+++ src/index.php
@@ -0,0 +1,17 @@
+<DOCTYPE html>
+<html>
+    <head>
+        <title> Library Home</title>
+    </head>
+
+    <body>
+        <img src="imgs/library_img.avif" width="500" height="500">
+        <br>
+        <a href="pages/BookInsertion.php"> Book Insertion </a>
+        <br>
+        <a href="pages/ViewBooks.php"> View Books </a>
+        <br>
+        <a href="pages/SearchBook.php"> Search Book </a>    
+        <br>
+    </body>
+</html>
