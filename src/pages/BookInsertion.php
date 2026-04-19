854353aed8d006dd82e8f5c449a7dbb09267b776
Vittorione <vittorio.tiozzo.dev@gmail.com>
2026-04-19 23:20:18 +0200

    implemented BookInsertion.php

---
+++ src/pages/BookInsertion.php
@@ -0,0 +1,38 @@
+<html>
+    <head>
+        <title> Book Insertion </title>
+        <p><a href="../index.php"> Return to the homepage </a></p>
+    </head>
+    <body>
+        <form action="../actions/saveBook.php" method="post">
+            <p>
+                Title:<br>
+                <input type="text" name="title">
+            </p>
+
+            <p>
+                Price:<br>
+                <input type="text" name="price">
+            </p>
+
+            <p>
+                Author:<br>
+                <?php
+                    require "../actions/connection.php";
+                    $sql_Author = "SELECT * FROM Author";
+                    $authors = $conn->query($sql_Author);
+
+                    echo "<select name='idAuthor' id='idAuthors'>";
+                    while($row = $authors->fetch_assoc()) {
+                        echo "<option value={$row['idAuthor']}> {$row['nameAuthor']} {$row['surnameAuthor']} </option>";
+                    }
+                    echo "</select>";
+                ?>
+            </p>
+
+            <p>
+                <input type="submit" value="Enter">
+            </p>
+        </form>
+    </body>
+</html>
